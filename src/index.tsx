f9fbf460a0bcdfba6f630644f4d001b15a8ab0c5
Zane Hitchcox <zwhitchcox@gmail.com>
2019-12-04 15:56:54 -0500

    text and select

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,21 @@ import styles from "./styles.css"
 import { createInput } from "@zecos/inputs"
 
 import TextField from "@material-ui/core/TextField"
+import { Select, MenuItem, FormControl, InputLabel } from '@material-ui/core'
+
+
+const getErrorInfo = state => {
+  const {errors, touched} = state
+  const hasErrors = Boolean(errors.length)
+  let errInfo = {}
+  if (touched && hasErrors) {
+    errInfo = {
+      error: true,
+      helperText: errors[0].toString(),
+    }
+  }
+  return errInfo
+}
 
 export const text = createInput(({helpers, props, state}) => {
   const {
@@ -12,21 +27,13 @@ export const text = createInput(({helpers, props, state}) => {
     id,
   } = helpers
   
-  const { errors, value, touched } = state
+  const { value } = state
 
-  const hasErrors = Boolean(errors.length)
-  let errInfo = {}
-  if (touched && hasErrors) {
-    errInfo = {
-      error: true,
-      helperText: errors[0].toString(),
-    }
-  }
 
   return (
     <div className={styles.textFieldContainer}>
       <TextField
-        {...errInfo}
+        {...getErrorInfo(state)}
         label={label}
         id={id}
         margin="normal"
@@ -38,4 +45,34 @@ export const text = createInput(({helpers, props, state}) => {
       />
     </div>
   )
+})
+
+const renderOption = ([label, value]) => (
+  <MenuItem value={value}>{label}</MenuItem>
+)
+export const select = createInput(({helpers, props, state}) => {
+  const {
+    onChange,
+    onBlur,
+    label,
+    id,
+  } = helpers
+  
+  const { options, ...moreProps } = props
+  
+  const { value } = state
+
+  return <FormControl fullWidth margin="normal">
+    <InputLabel id={`${id}-label`}>{label}</InputLabel>
+    <Select
+      id={id}
+      onChange={onChange}
+      value={value}
+      onBlur={onBlur}
+      fullWidth
+      {...moreProps}
+    >
+      {Object.entries(options).map(renderOption)}
+    </Select>
+  </FormControl>
 })
