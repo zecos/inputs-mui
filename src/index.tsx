bccfd0b8fabdbddbb63f4701a8d2bad8a28197ca
Zane Hitchcox <zwhitchcox@gmail.com>
2019-12-04 13:59:05 -0500

    add text

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,41 @@
-/**
- * @class ExampleComponent
- */
-
 import * as React from 'react'
+import styles from "./styles.css"
+import { createInput } from "@zecos/inputs"
 
-import styles from './styles.css'
+import TextField from "@material-ui/core/TextField"
 
-export type Props = { text: string }
+export const text = createInput(({helpers, props, state}) => {
+  const {
+    onChange,
+    onBlur,
+    label,
+    id,
+  } = helpers
+  
+  const { errors, value, touched } = state
 
-export default class ExampleComponent extends React.Component<Props> {
-  render() {
-    const {
-      text
-    } = this.props
-
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
+  const hasErrors = Boolean(errors.length)
+  let errInfo = {}
+  if (touched && hasErrors) {
+    errInfo = {
+      error: true,
+      helperText: errors[0].toString(),
+    }
   }
-}
+
+  return (
+    <div className={styles.textFieldContainer}>
+      <TextField
+        {...errInfo}
+        label={label}
+        id={id}
+        margin="normal"
+        onChange={onChange}
+        onBlur={onBlur}
+        value={value}
+        fullWidth
+        {...props}
+      />
+    </div>
+  )
+})
